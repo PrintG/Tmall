69549f4428a64477890b69fd1bfe04793c144260
PrintG <2662256509@qq.com>
2017-07-18 22:03:42 +0800

    天猫首页_v1.0.2

--- js/index.js
+++ js/index.js
@@ -72,7 +72,13 @@
                 $pic.eq($index).addClass("show");
                 $tab.eq($index).addClass("cur");
             }
-            autoPlay();
+			var BannerTimer = setInterval(autoPlay, 3000);	//自动轮播定时器
+			//移入暂停轮播,移出播放轮播
+			$Banner.hover(function(){
+				clearInterval(BannerTimer);
+			},function(){
+				BannerTimer = setInterval(autoPlay, 3000);
+			});
             //自动播放轮播
             function autoPlay(){
                 change(function(){
@@ -80,9 +86,84 @@
                     $index %= 6;
                 });
             }
+        })();
+
+        //商品列表部分
+        (function(){
+            var $wrap = $main.find(".Banner .content .wrap"),
+                $showListLi = $wrap.find(".showList li"),
+                $secondaryMenuLi = $wrap.find(".secondaryMenu li");
+
+            $showListLi.hover(function(){
+                var This = $(this);
+                This.addClass("active");
+                $secondaryMenuLi.eq(This.index()).addClass("show");
+            },function(){
+                var This = $(this);
+                This.removeClass("active");
+                $secondaryMenuLi.eq(This.index()).removeClass("show");
+            });
+
+            $secondaryMenuLi.hover(function(){
+                var This = $(this);
+                This.addClass("show");
+                $showListLi.eq(This.index()).addClass("active");
+            },function(){
+                var This = $(this);
+                This.removeClass("show");
+                $showListLi.eq(This.index()).removeClass("active");
+            });
 
         })();
 
     })();
+
+	//wrap 部分
+	(function(){
+		var $wrap = $(".wrap");
+
+		//右侧边栏部分
+		(function(){
+			var $container = $wrap.find(".slidebar .container"),
+				$items = $container.find(".items"),
+				$submenu = $items.find(".submenu"),
+				$length = $items.length-2;	//排除最后两个Items(需要单独注册事件)
+			
+			function Hover($items, $submenu){
+				this.$item = $items;
+				this.$submenu = $submenu;
+			}
+			//启动程序
+			Hover.prototype.exe = function(){
+				this.hover();
+			};
+			//注册一个公用的功能函数
+			Hover.prototype.hover = function(){
+				var This = this;
+				//为Item注册事件
+				this.$item.hover(function(){
+					//移入
+					This.$submenu.stop().show().delay(100).queue(function(){
+						This.$submenu.stop();
+						This.$submenu.addClass("show");
+					});
+				},function(){
+					//移出
+					This.$submenu.stop().removeClass("show").delay(500).queue(function(){
+						This.$submenu.stop();
+						This.$submenu.hide();
+					});
+				});
+			};
+			
+			for(var i = 0; i < $length; i ++){
+				var items = new Hover($items.eq(i),$submenu.eq(i));
+				items.exe();
+			}
+
+
+			
+		})();
+	})();
 
 })();
